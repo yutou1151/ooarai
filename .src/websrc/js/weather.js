f5b16584428631ea547a9d8e73454042219b7d8a
曾鼋棨 <yutou.1151.commu.reg@mail.ru>
2022-07-26 13:26:29 +0800

    Update weather.js

--- .src/websrc/js/weather.js
+++ .src/websrc/js/weather.js
@@ -1,12 +1,11 @@
-let xmlhttp=new XMLHttpRequest();
+let xmlhttp = new XMLHttpRequest();
 
 xmlhttp.open
-("get","https://devapi.qweather.com/v7/weather/now?key=0d5f7fe72fed436ba39649f69061273a&location=140.85,36.31")
+    ("get", "https://devapi.qweather.com/v7/weather/now?key=0d5f7fe72fed436ba39649f69061273a&location=140.85,36.31")
 xmlhttp.send();
-xmlhttp.onreadystatechange=()=>{
-    if(xmlhttp.readyState==4){
-        if(xmlhttp.status==200||
-xmlhttp.status==304){
+xmlhttp.onreadystatechange = () => {
+    if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200 || xmlhttp.status == 304) {
             console.log(JSON.parse(xmlhttp.responseText).now.temp);
             const data = JSON.parse(xmlhttp.responseText)
             Object.keys(data.now).forEach(key => {
@@ -14,7 +13,7 @@ xmlhttp.status==304){
                     document.getElementById(key).innerHTML = data.now[key];
                 }
             })
-            
+
         }
     }
 }
